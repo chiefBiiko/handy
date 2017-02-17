300775ec4b3df950808891cb3f61fbc0e83883dd
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-17 11:16:46 +0100

    made inBando robust

--- handy.R
+++ handy.R
@@ -4,13 +4,22 @@ R.version  # Info on running R version
 rstudioapi::getVersion()  # Info on running RStudio version
 sessionInfo()  # Session info
 .packages(all.available=T)  # Have a look at all available packages
+.libPaths()  # Get the paths 2 R modules and packages
 ls(getNamespace('sys'), all.names=T)  # Have a look at all namespaces in a package
-args(readline)  # Get the argument list of a function
 ls()  # Get all namespaces in a specified environment
 rm(list=ls())  # Clear all namespaces in the current environment
 readline('What\'s ur goal 4 today? ')  # Get user input from the console
 rstudioapi::askForPassword('What\'s the secret?')  # Ask with a popup in RStudio
 rstudioapi::sendToConsole('419 * 2', execute=F)  # Send code to the RStudio console
+args(readline)  # Get the argument list of a function
+
+inBando <- function(dir=NULL) {
+  # Checks whether u r in the specified working directory
+  # @param {character} dir Name of directory to be checked 4
+  # @return {bool} Do input and cwd match?
+  if (missing(dir)) stop('no input!')
+  return(grepl(paste0(dir, '$'), getwd(), ignore.case=T))
+}
 
 gothub <- function(remote, flname=sub('^.*/([^/]*)$', '\\1', remote), open=T) {
   # Download and open a .R or .Rmd file from a Github repo with 1 R function call!
